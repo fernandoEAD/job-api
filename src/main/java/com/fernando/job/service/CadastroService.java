cfc56caf581812339606de51156aad061f2bd1eb
fernandoEAD <fernando-danatas@hotmail.com>
2022-05-03 17:35:04 -0300

    DELETE para Cadastro

--- src/main/java/com/fernando/job/service/CadastroService.java
+++ src/main/java/com/fernando/job/service/CadastroService.java
@@ -38,4 +38,9 @@ public class CadastroService {
 		obj.setJob(objDto.getJob());
 		return repository.save(obj);
 	}
+
+	public void delete(Integer id) {
+		findById(id);
+		repository.deleteById(id);
+	}
 }
